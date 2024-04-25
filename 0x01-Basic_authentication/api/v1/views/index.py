6a32e17afcb95bb694157d5cabd030dcb3634aac
254jack <taixonj@gmail.com>
2024-04-25 21:55:08 +0300

    update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,11 +5,22 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """_summary_
+    """
+    abort(401, description='Unauthorized')
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """_summary_
+    """
+    abort(403, description='Forbidden')
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
-    Return:
-      - the status of the API
     """
     return jsonify({"status": "OK"})
 
@@ -17,8 +28,6 @@ def status() -> str:
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
-    Return:
-      - the number of each objects
     """
     from models.user import User
     stats = {}
